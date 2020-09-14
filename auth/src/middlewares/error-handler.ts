a9f52337b5e7fdeccf2bee5b46069667f07a47f4
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-14 18:03:35 +0300

    added error handling middleware

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log("something went wrong", error);
+  res.status(400).send({ message: "something went wrong" });
+};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,23 @@
 import { Request, Response, Router } from "express";
+import { body, validationResult } from "express-validator";
 const route = Router();
 
-route.post("/api/users/sigup", (req: Request, res: Response): void => {});
+route.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("please enter a valid email"),
+    body("password")
+      .isLength({ min: 6, max: 20 })
+      .withMessage("password must have 6 characters minimum and 20 maximum")
+  ],
+  (req: Request, res: Response): void => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(401).send({ message: errors.array()[0].msg });
+      return;
+    }
+    res.send({});
+  }
+);
 
 export { route as signUpRouter };
