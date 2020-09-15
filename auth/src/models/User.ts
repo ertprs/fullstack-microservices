0030f78bb9ec4a794d5591097e0bfca83e196669
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-15 12:45:49 +0300

    wired up user to signup

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -5,22 +5,30 @@ interface UserAttrs {
   password: string;
 }
 
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): mongoose.Document;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
 }
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-});
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
 
-const User = mongoose.model<any, UserModel>("User", UserSchema);
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model<UserDoc, UserModel>("User", UserSchema);
 
 UserSchema.statics.build = (attrs: UserAttrs): mongoose.Document => {
   return new User(attrs);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,7 +2,13 @@ import { Request, Response, Router } from "express";
 import { body, validationResult } from "express-validator";
 import { DatabaseConnectionError } from "../errors/Database-connection-error";
 import { RequestValidationError } from "../errors/Request-validation-error";
+import { User } from "../models/User";
 const route = Router();
+
+interface SignUp {
+  email: string;
+  password: string;
+}
 
 route.post(
   "/api/users/signup",
@@ -12,12 +18,27 @@ route.post(
       .isLength({ min: 6, max: 20 })
       .withMessage("password must have 6 characters minimum and 20 maximum")
   ],
-  (req: Request, res: Response): void => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+      }
+      const { email, password } = req.body as SignUp;
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        res.send({});
+        return;
+      }
+
+      // HASH PASSWORD
+
+      const user = User.build({ email, password });
+      await user.save();
+      res.status(201).send(user);
+    } catch (error) {
+      throw new DatabaseConnectionError();
     }
-    throw new DatabaseConnectionError();
   }
 );
 
