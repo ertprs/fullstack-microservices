2e95fa5cd96ddf1c0c3f99d88f24359445de9f2a
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-15 13:24:36 +0300

    added password hashing

--- auth/src/errors/BadRequestError.ts
+++ auth/src/errors/BadRequestError.ts
@@ -5,6 +5,7 @@ export class BadRequestError extends CustomError {
   statusCode: number = 401;
   constructor(public message: string) {
     super(message);
+    Object.setPrototypeOf(this, BadRequestError.prototype);
   }
   serializeErrors = (): ErrorResult[] => {
     return [{ message: this.message }];
---
+++ auth/src/services/Password.ts
@@ -0,0 +1,11 @@
+import { scrypt, randomBytes } from "crypto";
+import { promisify } from "util";
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+  static toHash = async (password: string): Promise<string> => {
+    const salt = randomBytes(8).toString("hex");
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+    return `${buf.toString("hex")}.${salt}`;
+  };
+}
