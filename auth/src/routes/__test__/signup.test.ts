57b5f1756e67d85c33d6f49c94b2fc2d67551ceb
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-17 00:01:00 +0300

    added first test

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -4,6 +4,6 @@ import { app } from "../../app";
 it("returns a status of 201 on success", async (): Promise<request.Test> => {
   return request(app)
     .post("/api/users/signup")
-    .send({ email: "test@test.com", password: "password" })
+    .send({ email: "test@test.com", password: "password " })
     .expect(201);
 });
