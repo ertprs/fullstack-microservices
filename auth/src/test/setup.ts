79b05d8c7930345d5dc775798ecb2947ced8d681
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-16 18:09:09 +0300

    added test setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,33 @@
+import mongoose,  from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongo: MongoMemoryServer;
+
+beforeAll(
+  async (): Promise<void> => {
+    const mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  }
+);
+
+beforeEach(
+  async (): Promise<void> => {
+    const collections = await mongoose.connection.db.collections();
+    collections.forEach(
+      async (collection): Promise<void> => {
+        await collection.deleteMany({});
+      }
+    );
+  }
+);
+
+afterAll(
+  async (): Promise<void> => {
+    await mongo.stop();
+    await mongoose.connection.close();
+  }
+);
