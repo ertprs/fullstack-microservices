37af3955c9753bacad99e04f7e5a06043bd20516
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-16 14:34:22 +0300

    added auth middleware

---
+++ auth/src/middlewares/auth.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+import { NotAuthorizedError } from "../errors/NotAuthorizedError";
+
+export const auth = (req: Request, res: Response, next: NextFunction): void => {
+  if (!req.currentUser) {
+    throw new NotAuthorizedError();
+  }
+  next();
+};
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,18 +1,14 @@
 import { Request, Response, Router } from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 const route = Router();
 
-route.get("/api/users/currentuser", (req: Request, res: Response): void => {
-  if (!req.session || !req.session.jwt) {
-    res.send({ currentUser: null });
-    return;
+route.get(
+  "/api/users/currentuser",
+  currentUser,
+  (req: Request, res: Response): void => {
+    res.send({ currentUser: req.currentUser || null });
   }
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (error) {
-    res.send({ currentUser: null });
-  }
-});
+);
 
 export { route as currentUserRouter };
