0d768b088fe48d77b48110d734fb57cc9c995bc7
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-16 13:48:16 +0300

    added signing out a user

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,6 +1,9 @@
 import { Request, Response, Router } from "express";
 const route = Router();
 
-route.post("/api/users/signout", (req: Request, res: Response): void => {});
+route.post("/api/users/signout", (req: Request, res: Response): void => {
+  req.session = null;
+  res.send({});
+});
 
 export { route as signOutRouter };
