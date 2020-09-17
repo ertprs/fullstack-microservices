93b48b75197daeeb77af58e5209bff84dfde8720
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-17 18:58:27 +0300

    added the userequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,20 +4,24 @@ import axios from "axios";
 const signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
   return (
     <div className="container">
       <form
         onSubmit={async e => {
           e.preventDefault();
+          try {
+            const res = await axios.post("/api/users/signup", {
+              email,
+              password
+            });
 
-          const res = await axios.post("/api/users/signup", {
-            email,
-            password
-          });
-
-          console.log(res.data);
-          setEmail("");
-          setPassword("");
+            console.log(res.data);
+            setEmail("");
+            setPassword("");
+          } catch (error) {
+            setErrors(error.response.data.errors);
+          }
         }}
       >
         <h1>Sign Up</h1>
@@ -25,7 +29,10 @@ const signup = () => {
           <label>Email Address</label>
           <input
             type="text"
-            onChange={e => setEmail(e.target.value)}
+            onChange={e => {
+              errors.length !== 0 && setErrors([]);
+              setEmail(e.target.value);
+            }}
             value={email}
             className="form-control"
           />
@@ -34,11 +41,23 @@ const signup = () => {
           <label>Password</label>
           <input
             type="password"
-            onChange={e => setPassword(e.target.value)}
+            onChange={e => {
+              errors.length !== 0 && setErrors([]);
+              setPassword(e.target.value);
+            }}
             value={password}
             className="form-control"
           />
         </div>
+        {errors.length !== 0 && (
+          <div className="alert alert-danger">
+            <h4>Oooops...</h4>
+            <ul className="my-0">
+              {errors.length !== 0 &&
+                errors.map(err => <li key={err.message}>{err.message}</li>)}
+            </ul>
+          </div>
+        )}
         <button className="btn btn-primary">Sign Up</button>
       </form>
     </div>
