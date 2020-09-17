ff818676ec8e81f2b1fe01765220926d6e8e1cd4
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-17 13:47:12 +0300

    wired up signing in tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("sets header cookie on succesful sign in", async (): Promise<void> => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" });
+  const res = await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@test.com", password: "password" });
+  expect(res.get("Set-Cookie")).toBeDefined();
+});
+it("expect 401 with invalid creds", async (): Promise<void> => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" });
+  await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@test.com", password: "passw" })
+    .expect(401);
+});
+it("expect 401 with invalid creds", async (): Promise<void> => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" });
+  await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@tes.com", password: "password" })
+    .expect(401);
+});
+it("expect 401 with no user with that email", async (): Promise<void> => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@tes.com", password: "password" })
+    .expect(401);
+});
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -7,11 +7,37 @@ it("returns a status of 201 on success", async (): Promise<request.Test> => {
     .send({ email: "test@test.com", password: "password " })
     .expect(201);
 });
-it("returns a status of 400 with invalid email", async (): Promise<
+it("returns a status of 401 with invalid email", async (): Promise<
   request.Test
 > => {
   return request(app)
     .post("/api/users/signup")
     .send({ email: "testtest.com", password: "password " })
     .expect(401);
+});
+it("returns a status of 401 with invalid email and password", async (): Promise<
+  request.Test
+> => {
+  return request(app).post("/api/users/signup").send({}).expect(401);
+});
+
+it("returns a status of 401 with email and password exists", async (): Promise<
+  request.Test
+> => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password " })
+    .expect(201);
+
+  return await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password " })
+    .expect(401);
+});
+it("sets a cookie on successful sign up", async (): Promise<void> => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password " })
+    .expect(201);
+  expect(response.get("Set-Cookie")).toBeDefined();
 });
