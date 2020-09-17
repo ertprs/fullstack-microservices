179a2f6a3005221f0e612a49f9f37fa67332b922
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-17 17:23:12 +0300

    wired up control element

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="container">
-      <h1>Sign Up</h1>
-      <div className="form-group">
-        <label>Email Address</label>
-        <input type="text" className="form-control" />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input type="password" className="form-control" />
-      </div>
-      <button className="btn btn-primary">Sign Up</button>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          console.log({ email });
+          console.log({ password });
+          setEmail("");
+          setPassword("");
+        }}
+      >
+        <h1>Sign Up</h1>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="text"
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Sign Up</button>
+      </form>
     </div>
   );
 };
