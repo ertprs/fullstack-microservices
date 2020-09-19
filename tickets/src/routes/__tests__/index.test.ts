4d0341df34d73e68e321f024ad2c79230dc7624c
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-19 20:30:44 +0300

    added a test to fetch all tickets

---
+++ tickets/src/routes/__tests__/index.test.ts
@@ -0,0 +1,19 @@
+import request from "supertest";
+import { app } from "../../app";
+import { signin } from "../../test/setup";
+
+const genTickets = async (): Promise<request.Test> => {
+  return await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signin())
+    .send({ title: "jsldhkjfshjk", price: 1232 })
+    .expect(201);
+};
+
+it("can fetch a list of tickets", async (): Promise<void> => {
+  await genTickets();
+  await genTickets();
+  await genTickets();
+  const res = await request(app).get("/api/tickets");
+  expect(res.body.length).toEqual(3);
+});
