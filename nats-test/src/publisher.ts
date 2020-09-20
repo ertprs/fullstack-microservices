93c5a2152c13c8d2a31e16046830f09389adf4a1
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-20 13:18:37 +0300

    published the first event

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -4,4 +4,12 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222"
 });
 
-stan.on("connect", () => console.log("publisher connected to NATS"));
+stan.on("connect", () => {
+  console.log("publisher connected to NATS");
+  const data = JSON.stringify({
+    id: "123",
+    title: "concert",
+    price: 20
+  });
+  stan.publish("ticket:created", data, () => console.log("event published"));
+});
