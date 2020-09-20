014b2b2bca48164e18935e3f4e4d7115b1c3e52a
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-20 14:29:14 +0300

    added manual ack mode to nats

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,13 +1,24 @@
-import nats from "node-nats-streaming";
+import { randomBytes } from "crypto";
+import nats, { Message } from "node-nats-streaming";
 
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222"
 });
 
+const options = stan.subscriptionOptions().setManualAckMode(true);
+
 stan.on("connect", (): void => {
   console.log("listener connected to NATS");
-  const subscription = stan.subscribe("ticket:created");
-  subscription.on("message", (msg): void => {
-    console.log("message received");
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "order-service-queue-group",
+    options
+  );
+  subscription.on("message", (msg: Message): void => {
+    const data = msg.getData();
+    if (typeof data === "string") {
+      console.log(`received event: ${msg.getSequence()}, with data: ${data}`);
+    }
+    msg.ack();
   });
 });
