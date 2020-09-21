4a5cd40e1bf8f0d96707cd5ac956e29fe71c5ca7
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-21 23:06:01 +0300

    scaffolding order service

---
+++ orders/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import "express-async-errors";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFound } from "@kmtickets/common";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(bodyParser.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+    sameSite: true
+  })
+);
+
+// NOT FOUND ROUTE
+app.all(
+  "*",
+  async (): Promise<void> => {
+    throw new NotFound();
+  }
+);
+
+// ERROR HANDLING
+app.use(errorHandler);
+
+export { app };
---
+++ orders/src/index.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./NatsWrapper";
+
+const start = async (): Promise<void> => {
+  try {
+    if (!process.env.JWT_KEY || !process.env.MONGO_URI) {
+      throw new Error("JWT_KEY MUST BE PROVIDED");
+    }
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI MUST BE PROVIDED");
+    }
+    if (!process.env.NATS_CLUSTER_ID) {
+      throw new Error("MONGO_URI MUST BE PROVIDED");
+    }
+    if (!process.env.NATS_URL) {
+      throw new Error("MONGO_URI MUST BE PROVIDED");
+    }
+    if (!process.env.NATS_CLIENT_ID) {
+      throw new Error("MONGO_URI MUST BE PROVIDED");
+    }
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", (): void => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", (): void => natsWrapper.client.close());
+    process.on("SIGTERM", (): void => natsWrapper.client.close());
+    await mongoose.connect(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("connected to db");
+    app.listen(3000, (): void => console.log(`listening on port 3000`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
