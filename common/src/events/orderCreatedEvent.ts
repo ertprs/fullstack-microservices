aab35bd569b3e9d12f007a50f3b35dacb7715194
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-22 18:30:32 +0300

    Updates

--- common/src/events/orderCreatedEvent.ts
+++ common/src/events/orderCreatedEvent.ts
@@ -8,7 +8,7 @@ export interface OrderCreatedEvent {
     status: OrderStatus;
     userId: string;
     expiresAt: string;
-    data: {
+    ticket: {
       id: string;
       price: number;
     };
