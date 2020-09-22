50fed4605f6e99db2e86d257ebbb7852021293f7
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-22 18:26:37 +0300

    Updates

--- common/src/events/BasePublisher.ts
+++ common/src/events/BasePublisher.ts
@@ -3,7 +3,7 @@ import { Subjects } from "./subjects";
 
 interface Event {
   subject: Subjects;
-  data: { [key: string]: string | number | { [key: string]: string | number } };
+  data: any;
 }
 
 export abstract class Publisher<T extends Event> {
