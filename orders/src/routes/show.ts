2d415f4032459f44d11e0b7a6c3e8b0719d85516
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-22 17:16:39 +0300

    added logic to fetch a specific order'

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,11 +1,21 @@
+import { auth, NotAuthorizedError, NotFound } from "@kmtickets/common";
 import { Router, Request, Response } from "express";
+import { Order } from "../models/order";
 
 const route = Router();
 
 route.post(
-  "/api/posts/:postId",
+  "/api/posts/:orderId",
+  auth,
   async (req: Request, res: Response): Promise<void> => {
-    res.send({});
+    const order = await Order.findById(req.params.orderId);
+    if (!order) {
+      throw new NotFound();
+    }
+    if (order.userId !== req.currentUser?.id) {
+      throw new NotAuthorizedError();
+    }
+    res.send(order);
   }
 );
 
