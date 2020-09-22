2592c74c95e95effa80a7f0f55fcabfa47113cb2
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-22 17:31:59 +0300

    added logic to cancel an order

--- orders/src/routes/__tests__/show.test.ts
+++ orders/src/routes/__tests__/show.test.ts
@@ -13,8 +13,24 @@ it("should fetch the order for a specific user", async (): Promise<void> => {
     .send({ ticketId: ticket.id })
     .expect(201);
 
-  await request(app)
+  const { body: fetchedOrder } = await request(app)
     .get(`/api/orders/${res.body.id}`)
     .set("Cookie", user)
     .expect(200);
+  expect(fetchedOrder.id).toEqual(res.body.id);
+});
+
+it("should not fetch another users order", async (): Promise<void> => {
+  const ticket = Ticket.build({ title: "ticket", price: 123 });
+  await ticket.save();
+  const user = signin();
+  const res = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  await request(app)
+    .get(`/api/orders/${res.body.id}`)
+    .set("Cookie", signin())
+    .expect(401);
 });
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,11 +1,29 @@
+import {
+  auth,
+  NotAuthorizedError,
+  NotFound,
+  OrderStatus
+} from "@kmtickets/common";
 import { Request, Response, Router } from "express";
+import { Order } from "../models/order";
 
 const route = Router();
 
 route.delete(
   "/api/orders/:orderId",
+  auth,
   async (req: Request, res: Response): Promise<void> => {
-    res.send({});
+    const order = await Order.findById(req.params.orderId);
+    if (!order) {
+      throw new NotFound();
+    }
+    if (order.userId !== req.currentUser?.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.send(order);
   }
 );
 
