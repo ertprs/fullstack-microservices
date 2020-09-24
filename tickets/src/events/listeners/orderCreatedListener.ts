bd2fb0f8f65ef0f72fe6c2bf5148e1ed1256742c
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-24 22:51:24 +0300

    added order created listener

---
+++ tickets/src/events/listeners/orderCreatedListener.ts
@@ -0,0 +1,9 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@kmtickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queueGroupName";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  protected subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  protected queueGroupName: string = queueGroupName;
+  onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
