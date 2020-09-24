4dff720a43fc80ea511302748d692428d2d48a80
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-25 01:06:44 +0300

    added updating ticket publisher when creating order listener

--- tickets/src/events/listeners/orderCreatedListener.ts
+++ tickets/src/events/listeners/orderCreatedListener.ts
@@ -1,6 +1,7 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@kmtickets/common";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticketUpdatedPublisher";
 import { queueGroupName } from "./queueGroupName";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -13,6 +14,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     }
     ticket.orderId = data.id;
     await ticket.save();
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      version: ticket.version,
+      orderId: ticket.orderId
+    });
     msg.ack();
   }
 }
