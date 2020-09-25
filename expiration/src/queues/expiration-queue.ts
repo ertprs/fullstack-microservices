4cf9a3738618387b3c95ce12073ebd99652d6344
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-25 13:36:57 +0300

    added an expitaion queue

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,21 @@
+import Queue from "bull";
+
+interface Payload {
+  orderId: string;
+}
+
+const expirationQueue = new Queue<Payload>("order:expiration", {
+  redis: {
+    host: process.env.REDIS_HOST
+  }
+});
+expirationQueue.process(
+  async (job): Promise<void> => {
+    console.log(
+      "I want to publish an expiration:complete event for order id",
+      job.data.orderId
+    );
+  }
+);
+
+export { expirationQueue };
